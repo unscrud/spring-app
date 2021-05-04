3718b4051d99a37f4736ae67bcced58f804c4731
Daniel Carvalho <daniel.devop@gmail.com>
2021-05-03 22:19:18 -0300

    Commit inicial

---
+++ src/main/java/com/unscrud/springapp/SpringAppApplication.java
@@ -0,0 +1,14 @@
+package com.unscrud.springapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAppApplication.class, args);
+		System.out.println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+	}
+
+}
