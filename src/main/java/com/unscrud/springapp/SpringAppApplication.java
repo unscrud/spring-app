d3ad88c78b8a56e82bf7ee1eca0e12c85b1071d8
Daniel Carvalho <daniel.devop@gmail.com>
2021-05-03 23:14:16 -0300

    Removendo Sout desnecessário

--- src/main/java/com/unscrud/springapp/SpringAppApplication.java
+++ src/main/java/com/unscrud/springapp/SpringAppApplication.java
@@ -8,7 +8,6 @@ public class SpringAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringAppApplication.class, args);
-		System.out.println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
 	}
 
 }
